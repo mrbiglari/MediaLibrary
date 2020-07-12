a890514c3ed2bfad9ca06e57547873397fae8b13
Mohammadreza Biglari <mb6714@ic.ac.uk>
2020-07-12 20:23:37 +0100

    Added EF MediaContext.

---
+++ src/MediaLibrary/MediaLibrary/Data/MediaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Data
+{
+    public class MediaContext : DbContext
+    {
+        public MediaContext(DbContextOptions<MediaContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MediaContext> Media { get; set; }
+    }
+}
