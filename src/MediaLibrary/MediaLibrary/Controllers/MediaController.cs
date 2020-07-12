26ef66cc90c1db8e7f08eaecb8b230e667e06dd9
Mohammadreza Biglari <mb6714@ic.ac.uk>
2020-07-12 17:42:31 +0100

    removed default API controller and model.

---
+++ src/MediaLibrary/MediaLibrary/Controllers/MediaController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<object>> GetAllCommmands()
+        {
+            return Ok();
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult<object> GetCommandById(int id)
+        {
+            return Ok();
+        }
+
+        //POST api/commands
+        [HttpPost]
+        public ActionResult<object> CreateCommand(object commandCreateDto)
+        {
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = 0 }, new object());
+        }
+
+        //PUT api/commands/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, object commandUpdateDto)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+            _repository.UpdateCommand(commandModelFromRepo);
+
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        //PATCH api/commands/{id}
+        [HttpPatch("{id}")]
+        public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<object> patchDoc)
+        {
+            return NoContent();
+        }
+
+        //DELETE api/commands/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        { 
+            return NoContent();
+        }
+    }
+}
