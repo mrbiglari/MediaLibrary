489522fe044268e8a31d79ef46a336ee19f55f21
Reza Biglari <reza@contrado.com>
2020-07-16 17:43:43 +0100

    media repository added

---
+++ src/MediaLibrary/MediaLibrary/Data/MediaRepository.cs
@@ -0,0 +1,32 @@
+﻿using MediaLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Data
+{
+    public class MediaRepository
+    {
+        private readonly MediaContext _context;
+
+        public MediaRepository(MediaContext context)
+        {
+            _context = context;
+        }
+        public MediaRepository()
+        {
+
+        }
+
+        public Media GetMedia(int id)
+        {
+            return _context.Media.Single(x => x.Id == id);            
+        }
+        public bool SaveMedia(Media media)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
